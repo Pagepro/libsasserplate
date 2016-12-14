b0a67a58d9c769feb05919d93962fa7119a3a7fd
Krzysztof Lojniewski <kontakt@pagepro.pl>
2016-12-14 20:55:12 +0100

    gulp commit added as pre commit hook

--- gulpfile.js/tasks/svgsprites.js
+++ gulpfile.js/tasks/svgsprites.js
@@ -1,15 +1,15 @@
-// var config       = require('../config')
-// var gulp      = require('gulp')
-// var svgSprite = require('gulp-svg-sprite')
-// var configSvg = {
-//     mode: {
-//         symbol: true,
-//         sprite: "sprite.<mode>.svg"
-//     }
-// }
+var config = require('../config')
+var gulp = require('gulp')
+var svgSprite = require('gulp-svg-sprite')
+var configSvg = {
+  mode: {
+    symbol: true,
+    sprite: 'sprite.<mode>.svg'
+  }
+}
 
-// gulp.task('svgsprites', function () {
-//     return gulp.src('**/*.svg', {cwd: config.root.src + '/svg'})
-//         .pipe(svgSprite(configSvg))
-//         .pipe(gulp.dest('static'));
-// });
+gulp.task('svgsprites', function () {
+  return gulp.src('**/*.svg', {cwd: config.root.src + '/svg'})
+        .pipe(svgSprite(configSvg))
+        .pipe(gulp.dest('static'))
+})
