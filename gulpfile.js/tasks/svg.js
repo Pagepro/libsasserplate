8cc4966fb734ea7661dc76395e368e15c9c6f426
Kuba Wiszniewski <xnante@gmail.com>
2019-02-08 11:35:29 +0100

    added gulp-svgmin, moved gulp-group-css-media-queries from dependencies to dev dependencies

---
+++ gulpfile.js/tasks/svg.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp')
+// const svgmin = require('gulp-svgmin')
+
+// const svgScript = () => gulp
+//   .src('logo.svg')
+//   .pipe(svgmin())
+//   .pipe(gulp.dest('./out'))
+
+// gulp.task('svg-script', svgScript)
+
+// module.exports = svgScript
