def2d48c1c023325bc54bbfe0ef85c2b22b82fb4
Kuba Wiszniewski <xnante@gmail.com>
2019-02-08 12:11:53 +0100

    added svgo process to svg sprites; added svg cleanup gulp process

--- gulpfile.js/tasks/svgsprites.js
+++ gulpfile.js/tasks/svgsprites.js
@@ -2,6 +2,7 @@ const config = require('../config')
 const gulp = require('gulp')
 const svgSprite = require('gulp-svg-sprite')
 const path = require('path')
+const svgmin = require('gulp-svgmin')
 const configSvg = {
   mode: {
     symbol: true,
@@ -15,6 +16,7 @@ const paths = {
 const svgSpritesTask = () => gulp
   .src('**/*.' + config.tasks.svgsprites.extensions, { cwd: paths.src })
   .pipe(svgSprite(configSvg))
+  .pipe(svgmin())
   .pipe(gulp.dest(path.join(global.production ? config.root.dist : '', paths.dest)))
 
 gulp.task('svgsprites', svgSpritesTask)
