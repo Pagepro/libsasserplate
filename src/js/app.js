06a25bc90ebbb3cfa586fd7e04f0f5afa58c592d
Kuba Wiszniewski <xnante@gmail.com>
2019-02-08 10:08:39 +0100

    cleaned base scripts - removed examples of utils and functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,4 @@
 import 'babel-polyfill'
-import './modules/example'
-import ClassExample from './modules/class-example'
 import svg4everybody from 'svg4everybody'
-import { getRandomNumber } from './utils'
 
 svg4everybody()
-
-/* ==========================================================================
- Exemplary class
-========================================================================== */
-const myClassExample = new ClassExample()
-myClassExample.init()
-
-console.log(getRandomNumber())
--- src/js/utils.js
+++
@@ -1,6 +0,0 @@
-// Keep your code DRY. If some functionality keeps repeating itself throughout the files, declare it here and import it everywhere it's used.
-const getRandomNumber = (max = 1) => Math.floor((Math.random() * (max + 1))) // generate a random number from 0 to max
-
-module.exports = {
-  getRandomNumber
-}
