0429453072576d9453d6ce42cf21161e5e9e7bfa
Kuba Wiszniewski <xnante@gmail.com>
2019-07-09 09:34:33 +0200

    fix paths for svg sprites and remove svgmin plugin that was doubling functionality and breaking the svg

--- gulpfile.js/tasks/svgsprites.js
+++ gulpfile.js/tasks/svgsprites.js
@@ -2,7 +2,6 @@ const config = require('../config')
 const gulp = require('gulp')
 const svgSprite = require('gulp-svg-sprite')
 const path = require('path')
-const svgmin = require('gulp-svgmin')
 const configSvg = {
   mode: {
     symbol: true,
@@ -16,7 +15,6 @@ const paths = {
 const svgSpritesTask = () => gulp
   .src('**/*.' + config.tasks.svgsprites.extensions, { cwd: paths.src })
   .pipe(svgSprite(configSvg))
-  .pipe(svgmin())
   .pipe(gulp.dest(path.join(global.production ? config.root.dist : '', paths.dest)))
 
 gulp.task('svgsprites', svgSpritesTask)
